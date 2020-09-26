06a28835d4a6974b80651454623ae6fce9797c4b
minji0426 <minjee0426@naver.com>
2020-09-26 13:00:05 +0900

    Add files via upload

---
+++ W04-P/author.php
@@ -0,0 +1,87 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', 'rootroot', 'kmj');
+
+  $query = "SELECT * FROM author";
+  $result = mysqli_query($link, $query);
+  $author_info = '';
+
+  while ($row = mysqli_fetch_array($result)) {
+      $filtered = array(
+      'id' => htmlspecialchars($row['id']),
+      'name' => htmlspecialchars($row['name']),
+      'profile' => htmlspecialchars($row['profile'])
+    );
+      $author_info .= '<tr>';
+      $author_info .= '<td>'.$filtered['id'].'</td>';
+      $author_info .= '<td>'.$filtered['name'].'</td>';
+      $author_info .= '<td>'.$filtered['profile'].'</td>';
+      $author_info .= '<td><a href="author.php?id='.$filtered['id'].'">update</a></td>';
+      $author_info .= '
+      <td>
+        <form action="process_delete_author.php" method="post">
+          <input type="hidden" name="id" value="'.$filtered['id'].'">
+          <input type="submit" value="delete">
+        </form>
+      </td>
+      ';
+      $author_info .= '</tr>';
+  };
+
+  $escaped = array(
+    'name' => '',
+    'profile' => ''
+  );
+
+  $form_action = 'process_create_author.php';
+  $label_submit = 'Create author';
+  $form_id = '';
+
+  if (isset($_GET['id'])) {
+      $filtered_id = mysqli_real_escape_string($link, $_GET['id']);
+      settype($filtered_id, 'integer');
+      $query = "SELECT * FROM author WHERE id = {$filtered_id}";
+      $result = mysqli_query($link, $query);
+      $row = mysqli_fetch_array($result);
+      $escaped['name'] = htmlspecialchars($row['name']);
+      $escaped['profile'] = htmlspecialchars($row['profile']);
+
+      $form_action = 'process_update_author.php';
+      $label_submit = 'Update author';
+      $form_id = '<input type="hidden" name="id" value="'.$_GET['id'].'">';
+  }
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> 닌텐도 게임 추천 </title>
+</head>
+<body>
+  <h1><a href="index.php"> 닌텐도 게임 추천 </a></h1>
+  <p><a href="index.php"> topic </a></p>
+
+  <table border="1">
+    <tr>
+      <th>id</th>
+      <th>name</th>
+      <th>profile</th>
+      <th>update</th>
+      <th>delete</th>
+    </tr>
+    <?= $author_info ?>
+  </table>
+  <br>
+  <form action="<?= $form_action ?>" method="post">
+    <?= $form_id ?>
+    <label for="fname">name:</label><br>
+    <input type="text" id="name" name="name" placeholder="name"
+    value="<?=$escaped['name']?>"><br>
+    <label for="lname">profile:</label><br>
+    <input type="text" id="profile" name="profile" placeholder="profile"
+    value="<?=$escaped['profile']?>"><br><br>
+    <input type="submit" value="<?=$label_submit?>">
+  </form>
+</body>
+</html>
---
+++ W04-P/index.php
@@ -0,0 +1,60 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', 'rootroot', 'kmj');
+  $query = "SELECT * FROM topic";
+  $result = mysqli_query($link, $query);
+  $list ='';
+
+  while ($row = mysqli_fetch_array($result)) {
+      $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+
+  $article = array(
+    'title' => '안녕하세요',
+    'description' => '집에서 시간을 보내는 요즘 추천하는 닌텐도 게임'
+  );
+  $update_link = '';
+  $delete_link = '';
+  $author = '';
+
+  if (isset($_GET['id'])) {
+      $filtered_id = mysqli_real_escape_string($link, $_GET['id']);
+      $query = "SELECT * FROM topic LEFT JOIN author ON
+      topic.author_id = author.id WHERE topic.id = {$filtered_id}";
+      $result = mysqli_query($link, $query);
+      $row = mysqli_fetch_array($result);
+      $article['title'] =
+      htmlspecialchars($row['title']);
+      $article['description'] =
+      htmlspecialchars($row['description']);
+      $article['name'] =
+      htmlspecialchars($row['name']);
+
+      $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
+      $delete_link = '
+      <form action ="process_delete.php" method="POST">
+        <input type="hidden" name="id" value="'.$_GET['id'].'">
+        <input type="submit" value="delete">
+      </form>
+      ';
+      $author = "<p>by {$article['name']}</p>";
+  }
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> 닌텐도 게임 추천 </title>
+</head>
+<body>
+  <h1><a href="index.php"> 닌텐도 게임 추천</a></h1>
+  <a href="author.php">author</a>
+  <ol> <?= $list ?> </ol>
+  <a href="create.php">create</a>
+  <?=$update_link?>
+  <?=$delete_link?>
+  <h2> <?= $article['title'] ?> </h2>
+  <?= $article['description'] ?>
+  <?= $author ?>
+</body>
+</html>
